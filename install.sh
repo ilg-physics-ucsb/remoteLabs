5852855570f1d901a57846e7883ef8420cecd244
Zak Espley <zespley@physics.ucsb.edu>
2020-07-14 18:44:20 -0700

    removed git clone. How else are they going to get install.sh?

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch 
 sudo apt update
 sudo apt install uv4l uv4l-uvc uv4l-server uv4l-webrtc uv4l-raspicam uv4l-raspicam-extras python3-pip git i2c-tools ufw -y
 pip3 install RPistepper pyvisa pyvisa-py adafruit-circuitpython-motorkit python-tplink-smarthome dlipower
-git clone https://github.com/ilg-physics-ucsb/remoteLabs.git
+#git clone https://github.com/ilg-physics-ucsb/remoteLabs.git
 #sudo mv ~/remoteLabs/setup/uv4l-uvc.conf /etc/uv4l
 sudo mv ~/remoteLabs/setup/uv4l-raspicam.conf /etc/uv4l
 sudo echo "# prepend the local file containing modules to the path that PYTHON searches for imports
