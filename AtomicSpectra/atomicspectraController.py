16a44126e945115f3d6f9823c542eb57652f4b0c
Zak Espley <zespley@physics.ucsb.edu>
2020-07-16 14:09:55 -0700

    Update atomicspectraController.py

--- AtomicSpectra/atomicspectraController.py
+++ AtomicSpectra/atomicspectraController.py
@@ -3,35 +3,22 @@ from labcontrol import Experiment, StepperI2C, Keithley6514Electrometer, Keithle
 import visa
 import pickle
 
-# resource_manager = visa.ResourceManager("@py")
-# visa_electrometer = resource_manager.open_resource('ASRL/dev/ttyUSB0::INSTR', baud_rate=57600)
-# visa_electrometer.read_termination = "\r\n"
-# visa_electrometer.write_termination = "\r\n"
-
-# visa_multimeter = resource_manager.open_resource('ASRL/dev/ttyUSB0::INSTR', baud_rate=19200) #not sure if USB# is unique 2004234
-# visa_multimeter.read_termination = "\r\n"
-# visa_multimeter.write_termination = "\r\n"
 
 camera = ArduCamMultiCamera("Camera", 1)
-# camera.camera("b")
 
 socket_path = "/tmp/uv4l.socket"
 
-#this uses the broadcom pin numbering system
-# oven_pins = [5,6,12,13]
-# filament_pins = [5,6,12,13]
-# Va_pins = [5,6,12,13]
-# Vr_pins = [5,6,12,13]
 
 slit = StepperI2C("Slit", 1,bounds=(-2100,2100), style="DOUBLE")  
 grating = StepperI2C("Grating", 2,bounds=(-2100,2100), style="DOUBLE")
 arm = StepperI2C("Arm", 3,bounds=(-2100,2100))
 carousel = StepperI2C("Carousel", 4,bounds=(-2100,2100))
 
+screen = ElectronicScreen
 
 
 
-ASDIpdu = PDUOutlet("ASDIpdu", "asdipdu.inst.physics.ucsb.edu", "admin", "raspberry", 60)
+ASDIpdu = PDUOutlet("ASDIpdu", "asdipdu.inst.physics.ucsb.edu", "admin", "5tgb567ujnb", 60)
 ASDIpdu.login()
 # OvenPower = Plug("OvenPower", "192.168.0.18")
 # FilamentPower = Plug("FilamentPower", "192.168.0.19")
