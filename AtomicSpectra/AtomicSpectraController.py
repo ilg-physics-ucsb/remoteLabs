0898157a56ad60cebfd2fe2af1940ca820516fa2
Zak Espley <zespley@gmail.com>
2020-07-21 22:57:23 -0700

    incomplete

--- AtomicSpectra/AtomicSpectraController.py
+++ AtomicSpectra/AtomicSpectraController.py
@@ -17,6 +17,8 @@ grating = StepperI2C("Grating", 2, bounds=(-450, 450), style="DOUBLE")
 arm = StepperI2C("Arm", 3,bounds=(-21000,21000), style="DOUBLE")
 carousel = StepperI2C("Carousel", 4,bounds=(-60, 13204), style="MICROSTEP", delay=0.00002, refPoints=refPoints)
 
+ambient = S
+
 
 ASDIpdu = PDUOutlet("ASDIpdu", "asdipdu.inst.physics.ucsb.edu", "admin", "5tgb567ujnb", 60, outlets=[6])
 ASDIpdu.login()
