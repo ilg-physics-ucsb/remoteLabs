283698a21ee9c45418ed50e7cf220a52bb08aa05
Deborah Fygenson <Deborah@169-231-132-90.wireless.ucsb.edu>
2020-08-01 23:15:29 -0700

    finalized motor delay times and set preliminary bounds in all controllers

--- DiffractionInterference/DiffractionInterferenceControllerAdmin.py
+++ DiffractionInterference/DiffractionInterferenceControllerAdmin.py
@@ -7,24 +7,28 @@ camera = ArduCamMultiCamera("Camera", 1)
 
 socket_path = "/tmp/uv4l.socket"
 
+Sstep = 610
+Mstep = 975
+Lstep = 1050
+
 refPointsSingle = {
     "SingleOpen": 0,
-    "LineSlit": 660,
-    "LittleHole": 2*660,
-    "BigHole": 3*660,
+    "LineSlit": Sstep,
+    "LittleHole": 2*Sstep+25,
+    "BigHole": 3*Sstep+75,
     # Blank
-    "A02": 3*660 + 975,
-    "A04": 4*660 + 975,
-    "A08": 5*660 + 975,
-    "A16": 6*660 + 975,
+    "A02": 3*Sstep + Mstep,
+    "A04": 4*Sstep + Mstep,
+    "A08": 5*Sstep + Mstep,
+    "A16": 6*Sstep + Mstep,
     # Blank
-    "VaryWidth": 6*660 + 2*975,
+    "VaryWidth": 6*Sstep + 2*Mstep,
     # Blank
-    "Square": 6*660 + 3*975 + 1050,
-    "Hex": 7*660 + 3*975 + 1050,
-    "Dots": 8*660 + 3*975 + 1050,
-    "Holes": 9*660 + 3*975 + 1050,
-
+    "Square": 6*Sstep + 3*Mstep + Lstep,
+    "Hex": 7*Sstep + 3*Mstep + Lstep,
+    "Dots": 8*Sstep + 3*Mstep + Lstep,
+    "Holes": 9*Sstep + 3*Mstep + Lstep,
+}
 refPointsMulti = {
     "TwoOne": 0,
     "FarClose": 0,
@@ -48,9 +52,11 @@ refPointsMulti = {
 screen = SingleGPIO("Screen", 26)
 ambient = SingleGPIO("Ambient", 5)
 
-multiSlits = StepperI2C("MultiSlits", 1, bounds=(-20000,20000), style="DOUBLE", refPoints=refPointsMulti)  #Multiple Slits
-singleSlits = StepperI2C("SingleSlits", 2,bounds=(-20000,20000), style="DOUBLE", refPoints=refPointsSingle) #Single Slits
-stage = StepperI2C("Stage", 4, bounds=(-20000, 200000), style="DOUBLE") #Screen
+bound = 1e6
+
+multiSlits = StepperI2C("MultiSlits", 1, bounds=(-bound,bound), style="DOUBLE", delay=0.00004, refPoints=refPointsMulti)  #Multiple Slits
+singleSlits = StepperI2C("SingleSlits", 2,bounds=(-bound,bound), style="DOUBLE", delay=0.00004, refPoints=refPointsSingle) #Single Slits
+stage = StepperI2C("Stage", 4, bounds=(-bound, bound), style="DOUBLE", delay=0.004) #Screen
 
 
 
