cec11a4f8c0b8fdf38b58700d89431d16762e3fd
RaffiShirinian <raffishirinian@gmail.com>
2020-08-03 21:06:16 -0700

    Carousel Motor Argument

--- AtomicSpectra/AtomicSpectraControllerAdmin.py
+++ AtomicSpectra/AtomicSpectraControllerAdmin.py
@@ -16,7 +16,7 @@ bound = int(1e6)
 slit = StepperI2C("Slit", 1,bounds=(-bound,bound), style="DOUBLE", delay=0.1)  
 grating = StepperI2C("Grating", 2, bounds=(-bound, bound), style="DOUBLE")
 arm = StepperI2C("Arm", 3,bounds=(-bound, bound), style="DOUBLE")
-carousel = StepperI2C("Carousel", 4,bounds=(-bound, bound), style="DOUBLE", delay=0.1, refPoints=refPoints)
+carousel = StepperI2C("Carousel", 4,bounds=(-bound, bound), style="DOUBLE", delay=0.1, refPoints=refPoints, microsteps=16)
 
 ambient = SingleGPIO("Ambient", 5)
 
