86d10d53649759888ebcf7198bb99c110beed3a7
Zak Espley <zespley@physics.ucsb.edu>
2020-08-06 16:38:42 -0700

    Made one controller to rule them all

--- PhotoElectricEffect/PhotoElectricEffectController.py
+++ PhotoElectricEffect/PhotoElectricEffectController.py
@@ -21,31 +21,26 @@ visa_multimeter.write_termination = "\r\n"
 
 socket_path = "/tmp/uv4l.socket"
 
-
-
-
 potentiometer = StepperI2C("Pot", 2, bounds=(0,2100))
 
 filterWheel = StepperI2C("Wheel", 1, bounds=(0,810), refPoints=refPoints)
 
 PEpdu = PDUOutlet("PEpdu", "128.111.18.80", "admin", "5tgb567ujnb", 60, outlets=outlets, outletMap=outletMap)
 PEpdu.login()
-# ambientLight = Plug("ambientLight", "192.168.0.3")
-# HgNeLamp = Plug("HgNeLamp", "192.168.0.18")
 
 electrometer = Keithley6514Electrometer("Electrometer", visa_electrometer)
-# electrometerPower = Plug("electrometerPower","192.168.0.19")
 
 multimeter = Keithley2000Multimeter("Multimeter", visa_multimeter)
-# multimeterPower = Plug("multimeterPower","192.168.0.20")
 
 
-exp = Experiment("PhotoElectricEffect")
+#RECALL-exp = Experiment("PhotoElectricEffect")
+#RESET-exp = Experiment("PhotoElectricEffect")
+#ADMIN-exp = Experiment("PhotoElectricEffect", admin=True)
 exp.add_device(PEpdu)
 exp.add_device(potentiometer)
 exp.add_device(filterWheel)
 exp.add_device(electrometer)
 exp.add_device(multimeter)
 exp.set_socket_path(socket_path)
-exp.recallState()
+#RECALL-exp.recallState()
 exp.setup()
