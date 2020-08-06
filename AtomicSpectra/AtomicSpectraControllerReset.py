df37f1d025d0341f11a4c1e6de057ff844f091c3
Zak Espley <zespley@physics.ucsb.edu>
2020-08-05 18:24:05 -0700

    First time Raffi and Zak code together. Friendship is on the rocks. Outlook looks bleak. Hopefully homing works. <3 Zak

--- AtomicSpectra/AtomicSpectraControllerReset.py
+++ AtomicSpectra/AtomicSpectraControllerReset.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-from labcontrol import Experiment, StepperI2C, Plug, PDUOutlet, ArduCamMultiCamera, SingleGPIO
+from labcontrol import Experiment, StepperI2C, Plug, PDUOutlet, ArduCamMultiCamera, SingleGPIO, LimitSwitch
 
 
 camera = ArduCamMultiCamera("Camera", 1)
@@ -12,9 +12,31 @@ refPoints = {
     "b": int(-2*78),
     }
 
+leftSwitch = LimitSwitch("LeftSwitch", 6)
+rightSwitch = LimitSwitch("RightSwitch", 12)
+homeSwitch = LimitSwitch("HomeSwitch", 13)
+
+def leftSwitchHit(motor, steps):
+    motor.adminMove(10)
+
+def rightSwitchHit(motor, steps):
+    motor.adminMove(-10)
+
+def homing(motor):
+    homeSwitch = motor.move(-20000)
+    if homeSwitch:
+        motor.adminMove(-5)
+    else:
+        motor.homeMove()
+
+leftSwitch.switchAction = leftSwitchHit
+rightSwitch.switchAction = rightSwitchHit
+
+
 slit = StepperI2C("Slit", 1,bounds=(0,600), style="DOUBLE", delay=0.1)  
 grating = StepperI2C("Grating", 2, bounds=(-450, 450), style="DOUBLE")
-arm = StepperI2C("Arm", 3,bounds=(-21000,21000), style="DOUBLE")
+arm = StepperI2C("Arm", 3,bounds=(-21000,21000), style="DOUBLE", limitSwitches=[leftSwitch, rightSwitch], homeSwitch=homeSwitch)
+arm.customHome = homing
 carousel = StepperI2C("Carousel", 4,bounds=(-161, 5), style="DOUBLE", delay=0.1, refPoints=refPoints, microsteps=16)
 
 ambient = SingleGPIO("Ambient", 5)
