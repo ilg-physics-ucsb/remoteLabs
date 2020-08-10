33c8dc5a1df558137720eb9d2d196127957152ad
RaffiShirinian <raffishirinian@gmail.com>
2020-08-10 11:56:41 -0700

    timer final fix?

--- PhotoElectricEffect/static/js/main.js
+++ PhotoElectricEffect/static/js/main.js
@@ -147,7 +147,7 @@ window.addEventListener('DOMContentLoaded', function () {
         var timer = duration, hours, minutes, seconds;
         setInterval(function () {
             hours = parseInt(timer / 3600, 10);
-            minutes = parseInt(timer % 60, 10);
+            minutes = parseInt(timer % 3600, 10);
             seconds = parseInt(timer % 60, 10);
     
             hours = hours <10 ? "0" + hours : hours;
