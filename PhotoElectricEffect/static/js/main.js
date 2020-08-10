a58d0a9e4552ae48391e79a46557e4fec38dee3d
RaffiShirinian <raffishirinian@gmail.com>
2020-08-10 12:47:36 -0700

    Hour Timer Fix

--- PhotoElectricEffect/static/js/main.js
+++ PhotoElectricEffect/static/js/main.js
@@ -146,9 +146,9 @@ window.addEventListener('DOMContentLoaded', function () {
     function startTimer(duration, display) {
         var timer = duration, hours, minutes, seconds;
         setInterval(function () {
-            hours = parseInt(timer / 3600, 10);
-            minutes = parseInt(timer % 3600, 10);
-            seconds = parseInt(timer % 60, 10);
+            hours = Math.floor(parseInt(timer / 3600, 10));
+            minutes = Math.floor(parseInt(timer % 3600 / 60 , 10));
+            seconds = Math.floor(parseInt(timer % 3600 % 60, 10));
     
             hours = hours <10 ? "0" + hours : hours;
             minutes = minutes < 10 ? "0" + minutes : minutes;
