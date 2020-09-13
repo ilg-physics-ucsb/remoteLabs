de98a0bca83ba568b7f7d83a6a77facc4d3242c2
Zak Espley <zespley@physics.ucsb.edu>
2020-09-12 20:29:11 -0700

    fixed spacing

--- DiffractionInterference/rtcTest/rtcTest.py
+++ DiffractionInterference/rtcTest/rtcTest.py
@@ -1,78 +1,78 @@
 from aiohttp import web
- routes = web.RouteTableDef()
+routes = web.RouteTableDef()
 from rtcbot import RTCConnection, getRTCBotJS, PiCamera
 
 # Initialize the camera
 camera = PiCamera()
 
  # For this example, we use just one global connection
- conn = RTCConnection()
+conn = RTCConnection()
 
 # Send images from the camera through the connection
 conn.video.putSubscription(camera)
 
- # Serve the RTCBot javascript library at /rtcbot.js
- @routes.get("/rtcbot.js")
- async def rtcbotjs(request):
-     return web.Response(content_type="application/javascript", text=getRTCBotJS())
+# Serve the RTCBot javascript library at /rtcbot.js
+@routes.get("/rtcbot.js")
+async def rtcbotjs(request):
+    return web.Response(content_type="application/javascript", text=getRTCBotJS())
 
 
- # This sets up the connection
- @routes.post("/connect")
- async def connect(request):
-     clientOffer = await request.json()
-     serverResponse = await conn.getLocalDescription(clientOffer)
-     return web.json_response(serverResponse)
+# This sets up the connection
+@routes.post("/connect")
+async def connect(request):
+    clientOffer = await request.json()
+    serverResponse = await conn.getLocalDescription(clientOffer)
+    return web.json_response(serverResponse)
 
- @routes.get("/")
- async def index(request):
-     return web.Response(
-         content_type="text/html",
-         text=r"""
-     <html>
-         <head>
-             <title>RTCBot: Skeleton</title>
-             <script src="/rtcbot.js"></script>
-         </head>
-         <body style="text-align: center;padding-top: 30px;">
-             <video autoplay playsinline controls></video> <audio autoplay></audio>
-             <p>
-             Open the browser's developer tools to see console messages (CTRL+SHIFT+C)
-             </p>
-             <script>
-                 var conn = new rtcbot.RTCConnection();
+@routes.get("/")
+async def index(request):
+    return web.Response(
+        content_type="text/html",
+        text=r"""
+    <html>
+        <head>
+            <title>RTCBot: Skeleton</title>
+            <script src="/rtcbot.js"></script>
+        </head>
+        <body style="text-align: center;padding-top: 30px;">
+            <video autoplay playsinline controls></video> <audio autoplay></audio>
+            <p>
+            Open the browser's developer tools to see console messages (CTRL+SHIFT+C)
+            </p>
+            <script>
+                var conn = new rtcbot.RTCConnection();
 
-+                // When the video stream comes in, display it in the video element
-+                conn.video.subscribe(function(stream) {
-+                    document.querySelector("video").srcObject = stream;
-+                });
+            // When the video stream comes in, display it in the video element
+            conn.video.subscribe(function(stream) {
+                document.querySelector("video").srcObject = stream;
+            });
 
-                 async function connect() {
-                     let offer = await conn.getLocalDescription();
+                async function connect() {
+                    let offer = await conn.getLocalDescription();
 
-                     // POST the information to /connect
-                     let response = await fetch("/connect", {
-                         method: "POST",
-                         cache: "no-cache",
-                         body: JSON.stringify(offer)
-                     });
+                    // POST the information to /connect
+                    let response = await fetch("/connect", {
+                        method: "POST",
+                        cache: "no-cache",
+                        body: JSON.stringify(offer)
+                    });
 
-                     await conn.setRemoteDescription(await response.json());
+                    await conn.setRemoteDescription(await response.json());
 
-                     console.log("Ready!");
-                 }
-                 connect();
+                    console.log("Ready!");
+                }
+                connect();
 
-             </script>
-         </body>
-     </html>
-     """)
+            </script>
+        </body>
+    </html>
+    """)
 
- async def cleanup(app):
+async def cleanup(app):
     await conn.close()
-    camera.close() # Singletons like a camera are not awaited on close
+camera.close() # Singletons like a camera are not awaited on close
 
- app = web.Application()
- app.add_routes(routes)
- app.on_shutdown.append(cleanup)
- web.run_app(app)
+app = web.Application()
+app.add_routes(routes)
+app.on_shutdown.append(cleanup)
+web.run_app(app)
