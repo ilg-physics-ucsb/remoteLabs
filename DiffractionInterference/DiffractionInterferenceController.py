3540cadbed8854d44855745bcc70e51d92ee399d
RaffiShirinian <raffishirinian@gmail.com>
2021-01-25 18:38:44 -0800

    SS Test

--- DiffractionInterference/DiffractionInterferenceController.py
+++ DiffractionInterference/DiffractionInterferenceController.py
@@ -58,7 +58,7 @@ socket_path = "/tmp/uv4l.socket"
 ambient = SingleGPIO("Ambient", ambientPin)
 
 multiSlits = StepperI2C("MultiSlits", 1, bounds=multiSlitBounds, style="DOUBLE", delay=0.00004, refPoints=refPointsMulti)  #Multiple Slits
-singleSlits = StepperI2C("SingleSlits", 2,bounds=singleSlitBounds, style="DOUBLE", delay=0.01, refPoints=refPointsSingle) #Single Slits
+singleSlits = StepperI2C("SingleSlits", 2,bounds=singleSlitBounds, style="MICROSTEP", delay=0.001, refPoints=refPointsSingle) #Single Slits
 stage = StepperI2C("Stage", 4, bounds=stageBounds, style="DOUBLE", delay=0.004) #Screen
 
 # Delay Old: delay=0.00004
