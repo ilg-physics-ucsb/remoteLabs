e54b95fd1dd3b68fe3c5585ead5032580aea60a4
RaffiShirinian <raffishirinian@gmail.com>
2021-02-01 15:55:46 -0800

    Microstep Update

--- DiffractionInterference/DiffractionInterferenceController.py
+++ DiffractionInterference/DiffractionInterferenceController.py
@@ -57,11 +57,12 @@ socket_path = "/tmp/uv4l.socket"
 # screen = SingleGPIO("Screen", 26)
 ambient = SingleGPIO("Ambient", ambientPin)
 
-multiSlits = StepperI2C("MultiSlits", 1, bounds=multiSlitBounds, style="DOUBLE", delay=0.00004, refPoints=refPointsMulti)  #Multiple Slits
+multiSlits = StepperI2C("MultiSlits", 1, bounds=multiSlitBounds, style="MICROSTEP", delay=0.001, refPoints=refPointsMulti)  #Multiple Slits
 singleSlits = StepperI2C("SingleSlits", 2,bounds=singleSlitBounds, style="MICROSTEP", delay=0.001, refPoints=refPointsSingle) #Single Slits
 stage = StepperI2C("Stage", 4, bounds=stageBounds, style="DOUBLE", delay=0.004) #Screen
 
 # Delay Old: delay=0.00004
+# Style Old: style="DOUBLE"
 # StepperI2C 'style' options are: 'SINGLE' 'DOUBLE' 'INTERLEAVE' 'MICROSTEP'
 # Default is 8 microsteps
 
