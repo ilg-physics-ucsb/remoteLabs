20bae653d5c8813486b0a2324a95cbaf913920ec
RaffiShirinian <raffishirinian@gmail.com>
2021-03-11 12:51:02 -0800

    Lab control

--- labcontrol/Controllers.py
+++ labcontrol/Controllers.py
@@ -826,7 +826,7 @@ class AbsorberController(MotorKit, BaseController):
 
 class Multiplexer(BaseController):
 
-    def __init__(self, name, pins, inhibitorPin, channels=[0,1,2,3,4,5,6,7] defaultChannel=None, defaultState=gpio.HIGH, delay=0.1):
+    def __init__(self, name, pins, inhibitorPin, channels=[0,1,2,3,4,5,6,7], defaultChannel=None, defaultState=gpio.HIGH, delay=0.1):
         self.pin = pin
         self.name = name
         self.experiment = None
