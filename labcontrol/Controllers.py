9ded2a20278cc4a2c3e9a74543b850860ef32ac7
Zak Espley <zespley@physics.ucsb.edu>
2021-04-22 15:30:04 -0700

    Adding return statement to pololu move method

--- labcontrol/Controllers.py
+++ labcontrol/Controllers.py
@@ -1022,6 +1022,12 @@ class PololuStepperMotor(BaseController):
         self.state['position'] = self.currentPosition
 
 
+        if self.currentPosition == self.upperBound or self.currentPosition == self.lowerBound:
+            return "{0}/{1}/{2}".format(self.name, "position", "limit")
+        else:
+            return "{0}/{1}/{2}".format(self.name, "position", self.currentPosition)
+
+
     def move_parser(self, params):
         if len(params) != 1:
             raise ArgumentNumberError(len(params), 1, "move")
