f59ce4116ac23cb2a6b6943207b87be860678034
ciortiz <ciortiz@ucsb.edu>
2022-03-03 18:50:20 -0800

    Commented out lines in franck1

--- FranckHertz/static/js/main.js
+++ FranckHertz/static/js/main.js
@@ -59,23 +59,23 @@ function onWebsocketMessage(message){
     alert(message);
 }
 
-function setupWebRTC(port, videoElement, vformat, hardwareCodec=false) {
-    var signalling_server_hostname = location.hostname || "192.168.0.2";
-    // var signalling_server_address = signalling_server_hostname + ':' + (port || (location.protocol === 'https:' ? 443 : 80));
-    var signalling_server_address = signalling_server_hostname + location.pathname + "ws"
-    var protocol = location.protocol === "https:" ? "wss:" : "ws:";
-    // var address = url + ':' + (port || (protocol === 'https:' ? 443 : 80)) + '/stream/webrtc';
-    // var address = location.hostname + ':' + (port || (protocol === 'https:' ? 443 : 80)) + '/stream/webrtc';
-    // protocol = "wss:";
-    // var address = url + "/webrtc";
-    var wsurl = protocol + '//' + signalling_server_address;
+// function setupWebRTC(port, videoElement, vformat, hardwareCodec=false) {
+//     var signalling_server_hostname = location.hostname || "192.168.0.2";
+//     // var signalling_server_address = signalling_server_hostname + ':' + (port || (location.protocol === 'https:' ? 443 : 80));
+//     var signalling_server_address = signalling_server_hostname + location.pathname + "ws"
+//     var protocol = location.protocol === "https:" ? "wss:" : "ws:";
+//     // var address = url + ':' + (port || (protocol === 'https:' ? 443 : 80)) + '/stream/webrtc';
+//     // var address = location.hostname + ':' + (port || (protocol === 'https:' ? 443 : 80)) + '/stream/webrtc';
+//     // protocol = "wss:";
+//     // var address = url + "/webrtc";
+//     var wsurl = protocol + '//' + signalling_server_address;
 
-    console.log(videoElement);
-    if (videoElement && videoElement.getAttribute('data-playing') == "false") {
-        var signalObj = new signal(wsurl, videoElement, vformat, hardwareCodec, connectStream, errorStream, closeStream, onWebsocketMessage)
-    }
-    return signalObj
-}
+//     console.log(videoElement);
+//     if (videoElement && videoElement.getAttribute('data-playing') == "false") {
+//         var signalObj = new signal(wsurl, videoElement, vformat, hardwareCodec, connectStream, errorStream, closeStream, onWebsocketMessage)
+//     }
+//     return signalObj
+// }
 
 var extremaModal, contactModal, bootModal
 
@@ -221,13 +221,13 @@ $("document").ready(function () {
 
     //for LiveFeed  
     // TEMP CHANGE
-    var mainCamSignal = setupWebRTC(8081, liveStream, 100);
+    //var mainCamSignal = setupWebRTC(8081, liveStream, 100);
  
     //for Time Limit
      window.setTimeout(timeOutHandler,10800000)
 
      function timeOutHandler(){
-         mainCamSignal.hangup()
+         //mainCamSignal.hangup()
          alert("Your session has timed out.")
      }
 
