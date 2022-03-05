d1a143cd5f611e76a52e14f7898dae3234b93fde
ciortiz <ciortiz@ucsb.edu>
2022-03-04 17:47:14 -0800

    edit1

--- labcontrol/Experiment.py
+++ labcontrol/Experiment.py
@@ -117,7 +117,7 @@ class Experiment(object):
     def command_handler(self, data):
         print("Data", data)
         data = data.decode('utf-8')
-        logging.info("Handling Command - " + data)
+        #logging.info("Handling Command - " + data)
         device_name, command, params = data.strip().split("/")
         params = params.split(",")
         print(device_name, command, params)
