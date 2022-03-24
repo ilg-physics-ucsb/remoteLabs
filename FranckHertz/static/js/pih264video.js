d33ffe2bd81b9255312e781a26ec143dc34e03c4
ciortiz <ciortiz@ucsb.edu>
2022-03-23 19:15:24 -0700

    edit on pih264video.js for wsurl

--- FranckHertz/static/js/pih264video.js
+++ FranckHertz/static/js/pih264video.js
@@ -1,8 +1,8 @@
 var signalling_server_hostname_video = location.hostname;
-var signalling_server_address_video = signalling_server_hostname_video + location.pathname + "ws3";
+var signalling_server_address_video = signalling_server_hostname_video + location.pathname;
 var protocol_video = location.protocol === "https:" ? "wss:" : "ws:";
 var port = 6049;
-var wsurl_video = protocol_video + '//' + signalling_server_address_video;
+var wsurl_video = protocol_video + '//' + signalling_server_address_video + ':' + port;
 // var wsurl_videotemp = 'ws://franck2.inst.physics.ucsb.edu:6049'
 window.onload = function(){	
     console.log("Attempting to connect to JMuxer")
