5db0b6c2ef9b24afd0ddb6f644fef2ddddcd7b06
Marlon Munoz <47518786+musicboyy@users.noreply.github.com>
2022-04-08 16:22:58 -0700

    Moved response_printer function before thread call

--- labcontrol/Experiment.py
+++ labcontrol/Experiment.py
@@ -119,6 +119,29 @@ class Experiment(object):
                 logging.error("Socket Error!", exc_info=True)
                 break
 
+    def response_printer(q):
+        
+        # Grabs response from queue
+        response = q.get() # Might need to handle case where nothing in queue
+        print("RESPONSE", response)
+
+        if response is not None:
+            print("Sending data")
+            # Carlos: comment line below and replace with your websocket sending features.
+            # Should take the response and send it to the client.
+
+            # Sends Response
+            self.connection.send(response.encode()) # back to whatever sent the command to socket
+        #Checks State
+        self.allStates[device_name] = self.devices[device_name].getState() #returns state variable
+        
+        #These will not work on a separate thread
+        # Two devices sending data at the same time
+
+        # Dumps into json file
+        with open(self.json_file, "w") as f:
+            json.dump(self.allStates, f)  
+
     def __data_connection(self, connection):
         # Defining queue that handles response between command_thread and response_thread
         response_queue = queue.Queue()
@@ -169,32 +192,8 @@ class Experiment(object):
 
         ### OLD Code
         #response = self.devices[device_name].cmd_handler(command, params)
-
         
-        
-
-    def response_printer(q):
-        
-        # Grabs response from queue
-        response = q.get() # Might need to handle case where nothing in queue
-        print("RESPONSE", response)
-
-        if response is not None:
-            print("Sending data")
-            # Carlos: comment line below and replace with your websocket sending features.
-            # Should take the response and send it to the client.
-
-            # Sends Response
-            self.connection.send(response.encode()) # back to whatever sent the command to socket
-        #Checks State
-        self.allStates[device_name] = self.devices[device_name].getState() #returns state variable
-        
-        #These will not work on a separate thread
-        # Two devices sending data at the same time
-
-        # Dumps into json file
-        with open(self.json_file, "w") as f:
-            json.dump(self.allStates, f)        
+           
 
     def exit_handler(self, signal_received, frame):
         # print("\r\nAttempting to exit")
