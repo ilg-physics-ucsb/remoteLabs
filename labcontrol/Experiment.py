85fb2d13a5195d946173fda3e537f21786571820
Marlon Munoz <47518786+musicboyy@users.noreply.github.com>
2022-04-15 17:32:30 -0700

    Fixed queue

--- labcontrol/Experiment.py
+++ labcontrol/Experiment.py
@@ -151,7 +151,7 @@ class Experiment(object):
         response_queue = queue.Queue()
 
         #Start response handling thread - CAN ONLY RUN ONCE AT A TIME
-        response_thread = threading.Thread(target=self.response_printer, args=(queue, ))
+        response_thread = threading.Thread(target=self.response_printer, args=(response_queue, ))
         response_thread.start()
 
 
