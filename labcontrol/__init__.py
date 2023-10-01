969c8337e05b583649b6990b980f92e2d5b25e28
Zak Espley <zespley@physics.ucsb.edu>
2023-10-01 15:13:44 -0700

    Update __init__.py
    Added Barry's new controllers to the __init__ for importing.

--- labcontrol/__init__.py
+++ labcontrol/__init__.py
@@ -1,2 +1,2 @@
 from .Experiment import Experiment
-from .Controllers import StepperSimple, StepperI2C, Keithley6514Electrometer,Keithley2000Multimeter,Plug, PDUOutlet, ArduCamMultiCamera, ElectronicScreen, SingleGPIO, LimitSwitch, DCMotorI2C, PushButton, Multiplexer, AbsorberController, PWMChannel, PololuDCMotor, PololuStepperMotor
+from .Controllers import StepperSimple, StepperI2C, Keithley6514Electrometer,Keithley2000Multimeter,Plug, PDUOutlet, ArduCamMultiCamera, ElectronicScreen, SingleGPIO, LimitSwitch, DCMotorI2C, PushButton, Multiplexer, AbsorberController, PWMChannel, PololuDCMotor, PololuStepperMotor, S42CStepperMotor, FS5103RContinuousMotor, GeneralPWMServo
