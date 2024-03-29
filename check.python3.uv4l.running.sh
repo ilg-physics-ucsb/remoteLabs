63c9350b787b5cbd077a0d08537c3de56ba5661e
physics-it <139639303+physics-it@users.noreply.github.com>
2024-03-29 13:50:16 -0700

    Fix check.python3.uv4l.running.sh

--- check.python3.uv4l.running.sh
+++ check.python3.uv4l.running.sh
@@ -2,12 +2,13 @@
 
 # check.python3.uv4l.running.sh
 # check if python3 and uv4l processes are running
-# rev3
+# rev4
 
 # add the line below to /etc/crontab to check every x minutes
 # */x * * * * root bash /home/pi/remoteLabs/check.python3.uv4l.running.sh
 
-if [ pgrep python3 >/dev/null 2>&1 && pgrep uv4l >/dev/null 2>&1 ]; then
+if pgrep python3 >/dev/null 2>&1 && pgrep uv4l >/dev/null 2>&1
+then
 	echo "check python3 & uv4l: running"
 	# sendemail -f from@example.com -t to@example.com_ -s smtp@example.org:25 -u "[REMOTELABSPSCHECK] $HOSTNAME OK" -m " "
 else
