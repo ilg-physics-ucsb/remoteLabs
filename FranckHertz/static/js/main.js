70254a9fd7b00929adf331d8e3a2726e2ef4e662
ZakEspley <zak.espley@gmail.com>
2025-06-01 16:09:28 -0700

    Removed old webRTC function

--- FranckHertz/static/js/main.js
+++ FranckHertz/static/js/main.js
@@ -1,30 +1,3 @@
-
-// This is the function that adds the video stream. You can have it do other things (like turn off a loading element) once it receives a stream.
-function connectStream(stream, videoElement) {
-    if (videoElement) {
-        console.log("got a stream! Putting stream in the following video" );
-        console.log(videoElement);
-        videoElement.srcObject = stream;
-        videoElement.setAttribute("data-playing", "true");
-
-        // videoElement.play();
-    }
-}
-
-//This function runs if there is an error returned from teh websocket connecting to the stream.
-function errorStream(error){
-    alert(error);
-}
-
-// This functions gets run when the websocket is closed.
-function closeStream(videoElement) {
-    if (videoElement) {
-        videoElement.srcObject = null;
-        videoElement.setAttribute("data-playing", "false");
-        console.log("websocket closed. bye bye!");
-    }
-}
-
 function controllerResponseHandler(cmd) {
     var components = cmd.split("/");
     var device = components[0]
@@ -59,30 +32,12 @@ function onWebsocketMessage(message){
     alert(message);
 }
 
-function setupWebRTC(port, videoElement, vformat, hardwareCodec=false) {
-    var signalling_server_hostname = location.hostname || "192.168.0.2";
-    // var signalling_server_address = signalling_server_hostname + ':' + (port || (location.protocol === 'https:' ? 443 : 80));
-    var signalling_server_address = signalling_server_hostname + location.pathname + "ws"
-    var protocol = location.protocol === "https:" ? "wss:" : "ws:";
-    // var address = url + ':' + (port || (protocol === 'https:' ? 443 : 80)) + '/stream/webrtc';
-    // var address = location.hostname + ':' + (port || (protocol === 'https:' ? 443 : 80)) + '/stream/webrtc';
-    // protocol = "wss:";
-    // var address = url + "/webrtc";
-    var wsurl = protocol + '//' + signalling_server_address;
-
-    console.log(videoElement);
-    if (videoElement && videoElement.getAttribute('data-playing') == "false") {
-        var signalObj = new signal(wsurl, videoElement, vformat, hardwareCodec, connectStream, errorStream, closeStream, onWebsocketMessage)
-    }
-    return signalObj
-}
-
 var extremaModal, contactModal, bootModal
 
 $("document").ready(function () {
     var stepPerDegree= 0.5; //This value is set by finalized mechanical arrangements.
     var currentPosition = 0;
-    var liveStream = document.getElementById("v");
+    var liveStream = document.getElementById("video");
     var FirstTimeOvenOn = true;
     var FirstTimeOvenOff = true;
     var FirstTimePSon = true;
@@ -221,13 +176,13 @@ $("document").ready(function () {
 
     //for LiveFeed  
     // TEMP CHANGE
-    var mainCamSignal = setupWebRTC(8081, liveStream, 100);
+    // var mainCamSignal = setupWebRTC(8081, liveStream, 100);
  
     //for Time Limit
      window.setTimeout(timeOutHandler,10800000)
 
      function timeOutHandler(){
-         mainCamSignal.hangup()
+         //mainCamSignal.hangup()
          alert("Your session has timed out.")
      }
 
@@ -815,7 +770,7 @@ $("document").ready(function () {
 
 window.addEventListener('beforeunload', function(e) {
     // TEMP CHANGE
-    mainCamSignal.hangup();
+    // mainCamSignal.hangup();
     // TEMP CHANGE
     dataChannel.close();
 })
