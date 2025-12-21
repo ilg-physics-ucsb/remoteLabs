436a0e899ff300618e7f7d1b2827de20df5a62cf
Clint Philbin <clint.philbin@gmail.com>
2025-12-21 15:15:20 -0800

    Create knob component

---
+++ static/js/components/knob.js
@@ -0,0 +1,110 @@
+export class Knob extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+
+    this.defaultImageUrl = '../PhotoElectricEffect/static/imgs/knob.png';
+
+    this.shadowRoot.innerHTML = `
+      <style>
+        .knob {
+          display: inline-flex;
+          flex-direction: column;
+        }
+
+        h3 {
+          text-align: center;
+        }
+
+        select {
+          margin-bottom: 8px;
+        }
+
+        .row {
+          display: flex;
+          align-items: center;
+          gap: 8px;
+        }
+
+        img {
+          width: 60px;
+          height: 60px;
+        }
+
+        button {
+          font-size: 24px;
+        }
+      </style>
+
+      <div class="knob">
+        <h3></h3>
+        <select>
+          <option value="" disabled>Sent interval</option>
+        </select>
+        <div class="row">
+          <button id="down">&#8634;</button>
+          <img id="image" src="${this.knobImageUrl}"/>
+          <button id="up">&#8635;</button>
+        </div>
+      </div>
+    `;
+  }
+
+  connectedCallback() {
+    const label = this.shadowRoot.querySelector('h3');
+    const downButton = this.shadowRoot.querySelector('#down');
+    const upButton = this.shadowRoot.querySelector('#up');
+    const intervalSelect = this.shadowRoot.querySelector('select');
+
+    this.componentId = this.getAttribute('component-id');
+    this.intervals = JSON.parse(this.getAttribute('intervals'));
+    this.selectedInterval = this.intervals[0];
+    this.knobImageUrl = this.getAttribute('knobImageUrl');
+
+    label.textContent = this.getAttribute('label');
+
+    if (this.intervals && this.intervals.length) {
+      this.intervals.forEach((interval, index) => {
+        const option = document.createElement('option');
+        option.value = interval;
+        option.innerHTML = interval;
+        if (index === 0) {
+          option.selected = true;
+        }
+
+        intervalSelect.appendChild(option);
+      });
+    }
+
+    const image = this.shadowRoot.querySelector('#image');
+    image.src = this.knobImageUrl ? this.knobImageUrl : this.defaultImageUrl;
+
+    downButton.addEventListener('click', () => {
+      this.down();
+    });
+
+    upButton.addEventListener('click', () => {
+      this.up();
+    });
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(`Attribute ${name} has changed.`);
+    this.knobImageUrl = newValue;
+  }
+
+  up() {
+    this.emit();
+  }
+
+  down() {
+    this.emit();
+  }
+
+  emit() {
+    const knobEvent = new CustomEvent('knob', { detail: { componentId: this.componentId, value: this.value } });
+    window.dispatchEvent(knobEvent);
+  }
+}
+
+customElements.define('knob-component', Knob);
