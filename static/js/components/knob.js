0c459b6d50530b8aaff2cdb0695007ffaf3723ba
Clint Philbin <clint.philbin@gmail.com>
2026-01-25 15:16:29 -0800

    Remove default interval selection

--- static/js/components/knob.js
+++ static/js/components/knob.js
@@ -39,11 +39,11 @@ export class Knob extends HTMLElement {
       <div class="knob">
         <h3></h3>
         <select>
-          <option value="" disabled>Select an interval</option>
+          <option value="">Select an interval</option>
         </select>
         <div class="row">
           <button id="down">&#8634;</button>
-          <img id="image" src="${this.knobImageUrl}"/>
+          <img id="image"/>
           <button id="up">&#8635;</button>
         </div>
       </div>
@@ -58,18 +58,14 @@ export class Knob extends HTMLElement {
 
     this.componentId = this.getAttribute('component-id');
     this.intervals = JSON.parse(this.getAttribute('intervals'));
-    this.selectedInterval = this.intervals[0];
     this.knobImageUrl = this.getAttribute('knobImageUrl');
 
     label.textContent = this.getAttribute('label');
 
-    this.intervals.forEach((interval, index) => {
+    this.intervals.forEach((interval) => {
       const option = document.createElement('option');
       option.value = interval;
       option.innerHTML = interval;
-      if (index === 0) {
-        option.selected = true;
-      }
 
       intervalSelect.appendChild(option);
     });
@@ -91,10 +87,16 @@ export class Knob extends HTMLElement {
   }
 
   up() {
+    if (!this.selectedInterval) {
+      return;
+    }
     this.emit(+this.selectedInterval);
   }
 
   down() {
+    if (!this.selectedInterval) {
+      return;
+    }
     this.emit(-this.selectedInterval);
   }
 
