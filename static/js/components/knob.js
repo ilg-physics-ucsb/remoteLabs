70b294e5502d9c39ff5b1b9f97d6e56fbea7122c
Clint Philbin <clint.philbin@gmail.com>
2026-01-26 19:45:07 -0800

    Use svg for arrows and copy hover/select styles

--- static/js/components/knob.js
+++ static/js/components/knob.js
@@ -31,8 +31,25 @@ export class Knob extends HTMLElement {
           height: 60px;
         }
 
-        button {
-          font-size: 24px;
+        #up, #down {
+          width: 42px;
+          height: 97px;
+          cursor: pointer;
+        }
+
+        #up path, #down path {
+          stroke: #076426;
+          stroke-width: 11;
+          fill: #14A54B;
+          transition: fill 0.2s ease, fill-opacity 0.2s ease;
+        }
+
+        #up path:hover,
+        #down path:hover,
+        #up.selected path,
+        #down.selected path {
+          fill: bisque;
+          fill-opacity: 0.75;
         }
       </style>
 
@@ -42,9 +59,13 @@ export class Knob extends HTMLElement {
           <option value="">Select an interval</option>
         </select>
         <div class="row">
-          <button id="down">&#8634;</button>
+          <svg id="down" viewBox="0 0 298 722" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M238.5 93.5L169 0C72.5 70 5.5 197.5 5.5 320.5C5.5 414 35.5 502 103 590L55.5 632.5L290 713L244 473L195 511.5C163 478.5 125 406.07 125 320.5C125 241.5 169 153.5 238.5 93.5Z"/>
+          </svg>
           <img id="image"/>
-          <button id="up">&#8635;</button>
+          <svg id="up" viewBox="0 0 298 722" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M58.6875 93.5L128.188 0C224.688 70 291.688 197.5 291.688 320.5C291.688 414 261.688 502 194.188 590L241.688 632.5L7.1875 713L53.1875 473L102.188 511.5C134.188 478.5 172.188 406.07 172.188 320.5C172.188 241.5 128.188 153.5 58.6875 93.5Z"/>
+          </svg>
         </div>
       </div>
     `;
@@ -78,10 +99,14 @@ export class Knob extends HTMLElement {
     });
 
     downButton.addEventListener('click', () => {
+      upButton.classList.remove('selected');
+      downButton.classList.add('selected');
       this.down();
     });
 
     upButton.addEventListener('click', () => {
+      downButton.classList.remove('selected');
+      upButton.classList.add('selected');
       this.up();
     });
   }
