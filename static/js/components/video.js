191ca69afa65a0fd332f7962ec5a9cd056cc8d89
Clint Philbin <clint.philbin@gmail.com>
2026-03-29 14:54:01 -0700

    Style and temp webm for dev

--- static/js/components/video.js
+++ static/js/components/video.js
@@ -1,3 +1,14 @@
+/**
+ * Connects the video
+ */
+
+/**
+ * TODO
+ * camera selection (should emit message for application to pass to API) name: display in UI, id: API knows (zak)
+ * connect to video feed
+ * pixel measuring tool
+ * camera settings: exposure, brightness, contrast
+ */
 export class Video extends HTMLElement {
   constructor() {
     super();
@@ -9,16 +20,25 @@ export class Video extends HTMLElement {
           display: flex;
           justify-content: center;
           align-items: center;
+          height: 100%;
+        }
+
+        video {
+          width: 100%;
+          max-height: 100%;
+          height: 100%;
+          object-fit: contain;
         }
       </style>
 
       <div class="video">
-        <p>video placeholder<p>
+        <video id="video" autoplay src="https://6922136c-83f3-4f95-ad86-9ef1221337ac.mdnplay.dev/shared-assets/videos/flower.webm"></video>
       </div>
     `;
   }
 
   connectedCallback() {
+    this.views = JSON.parse(this.getAttribute('views'));
   }
 
   emit(value) {
